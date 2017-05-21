30ef4690ed44d88c52a26a8156355806d979eac2
Hemalatha Kottaisamy <khema2010@gmail.com>
2017-05-21 12:13:19 -0700

    Add the file

---
+++ static/js/movie.js
@@ -0,0 +1,166 @@
+$(document).ready(function(){
+	// Initialize Firebase
+	var config = {
+	apiKey: "AIzaSyCBMDlFFcJ0OtLP1zK5wdcPB8OXpjUyc3E",
+	authDomain: "sf-movie-1495312209329.firebaseapp.com",
+	databaseURL: "https://sf-movie-1495312209329.firebaseio.com",
+	projectId: "sf-movie-1495312209329",
+	storageBucket: "sf-movie-1495312209329.appspot.com",
+	messagingSenderId: "148800737602"
+	};
+	firebase.initializeApp(config);
+	// list of google map markers
+	var markers = [];
+
+	var searchByMovie = true;
+
+	var resultsContainer = $('[data-container=results]');
+
+	var titleParam = $('[data-query-param="title_name"]');
+
+	// generate info window template
+	function createInfoWindowContent(director, title, location) {
+		var contentString = 
+			'<div class = "text-capitalize">'+ "Movie: " + title + ', </div>' + 
+			'<div class = "text-capitalize">'+ "Directed By: " + director + ', </div>' + 
+			'<div class = "text-capitalize">'+ "Location: " + location + ", San Francisco, CA" + '.</div>';
+		return contentString;
+	}
+
+	// add markers and info windows in the map
+	function addMarker(result, option) {
+		var infowindow = new google.maps.InfoWindow();
+
+		var marker = new google.maps.Marker({
+		    position: {
+		    	lat: result.lat,
+		    	lng: result.lng
+		    },
+		    map: map,
+		    title: option === "director" ? result.director : result.title
+		});
+
+		markers.push(marker);
+
+		marker.addListener('click', function() {
+			infowindow.setContent('');
+			infowindow.close(infowindow);
+		    infowindow.setContent(createInfoWindowContent(result.director, 
+		   		result.title, result.location));
+			infowindow.open(map, marker);
+		 });
+
+		// close the infowindow when clicked anywhere else
+		map.addListener('click', function(){
+		    infowindow.close(infowindow);
+		    infowindow.marker = null;
+		});
+
+		// Recenter map upon window resize
+		google.maps.event.addDomListener(window, 'resize', function() {
+			var center = map.getCenter();
+			google.maps.event.trigger(map, 'resize');
+			map.setCenter(center);
+		});
+
+		//set infowindow
+		map.panTo(marker.position);
+		//pan down infowindow by 200px to keep whole infowindow on screen
+		map.panBy(0, -100);
+	}
+
+	$.when().then(function() {
+		var src = "{{#each titles}}" + 
+	      		  "<li><a class = 'text-capitalize' href='#''>{{this}}</a></li>" +
+	      		  "{{/each}}";
+
+		var resultsTemplate = Handlebars.compile(src);
+		$.ajax({
+			url: '/json/titles',
+			data: {
+				name: 'a'
+			},
+			method: 'GET',
+			success: function (jsonResponse) {
+				var tableHtml = resultsTemplate({
+					titles: jsonResponse.titles
+				});
+				resultsContainer.html(tableHtml)
+				resultsContainer.show();
+
+				markers.forEach(function (marker) {
+					marker.setMap(null);
+				});
+				markers = [];
+				
+				if(jsonResponse.data && jsonResponse.data.length > 0) {
+					jsonResponse.data.forEach(function (result){
+						addMarker(result, "title");
+					});	
+				} else {
+					resultsContainer.hide();
+				}
+			},
+			error: function (err) {
+				//TODO
+			}
+		});
+	});
+
+
+	//event trigger for search by title and call titles public api
+	titleParam.keyup(function(event) {
+
+		var src = "{{#each titles}}" + 
+	      		  "<li><a class = 'text-capitalize' href='#''>{{this}}</a></li>" +
+	      		  "{{/each}}";
+
+		var resultsTemplate = Handlebars.compile(src);
+		$.ajax({
+			url: '/json/titles',
+			data: {
+				name: titleParam.val()
+			},
+			method: 'GET',
+			success: function (jsonResponse) {
+				var tableHtml = resultsTemplate({
+					titles: jsonResponse.titles
+				});
+				resultsContainer.html(tableHtml)
+				resultsContainer.show();
+
+				markers.forEach(function (marker) {
+					marker.setMap(null);
+				});
+				markers = [];
+				
+				if(jsonResponse.data && jsonResponse.data.length > 0) {
+					jsonResponse.data.forEach(function (result){
+						addMarker(result, "title");
+					});	
+				} else {
+					resultsContainer.hide();
+				}
+			},
+			error: function (err) {
+				//TODO
+			}
+		});
+	});	
+
+	//Display selected auto complete pins
+	resultsContainer.on('click', 'li', function (event) {
+		var title = $(event.target).text();
+		if (searchByMovie) {
+			titleParam.val(title);
+		} else {
+			directorParam.val(title);
+		}
+		markers.forEach(function (marker) {
+			if (marker.title !== title) {
+				marker.setMap(null);
+			}
+		resultsContainer.hide();
+		});
+	}); 
+});
