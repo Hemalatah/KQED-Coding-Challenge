2a329c0dbba90648fa18318c243936f16e4ae8f1
Hemalatha <khema2010@gmail.com>
2017-05-23 23:18:11 -0700

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -7,26 +7,6 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'SF Movies' });
 });
 
-router.get('/json/director', function(req, res) {
-    if(!req.query.name) return res.json({});
-
-    var directorParam = (req.query.name) ? req.query.name.toLowerCase() : '';
-
-    var uniqueList = {};
-    var filteredMovieArray = movies
-    .filter(function (movieObj) {
-        if(movieObj.director.indexOf(directorParam) === 0 && Object.keys(uniqueList).length <=10) {
-        uniqueList[movieObj.director] = true; 
-        return true;
-        }
-    });
-
-    res.json({
-    directors: Object.keys(uniqueList),
-    data: filteredMovieArray
-    });
-});
-
 router.get('/json/titles', function(req, res) {
     if(!req.query.name) return res.json({});
 
